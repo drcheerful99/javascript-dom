33cd95bf4a51d32f6e7b9a457cca07ada58e48fa
drcheerful99 <dekodable.consulting@gmail.com>
2021-02-15 21:04:24 -0500

    minor fix

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -33,12 +33,12 @@ button.on("click", function() {
     var filteredData = tableData.filter(dates => dates.datetime === inputValue);
 
     filteredData.forEach(function(selections) {
-    // console.log(selections);
+    console.log(selections);
     // Append one table row `tr` for each UFO sighting object
     var row = tbody.append("tr");
     // Use `Object.entries` to console.log each UFO sighting value
     Object.entries(selections).forEach(function([key, value]) {
-        // console.log(key, value);
+        console.log(key, value);
         // Append a cell (td) to the row for each value
         var cell = row.append("td");
         cell.text(value);
